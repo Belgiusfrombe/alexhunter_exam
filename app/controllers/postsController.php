3a256110301827ce917fa0f65da41b9cdc40ab4f
belgiusfrombe <alexandrearmanocs@outlook.fr>
2024-10-07 15:49:50 +0200

    5. ROUTE INSERT INTO

--- app/controllers/postsController.php
+++ app/controllers/postsController.php
@@ -39,3 +39,34 @@ function addFormAction(PDO $connexion){
     include '../app/views/posts/addForm.php';
     $content = ob_get_clean();
 }
+
+function addAction(PDO $connexion, array $data) {
+
+        include_once "../app/models/postsModel.php";
+        $id = PostsModel\createOne($connexion, $data);
+        header('Location: ' . BASE_PUBLIC_URL . 'posts.html');
+
+}
+function updateAction(PDO $connexion, int $id, array $data) {
+        include_once '../app/models/postsModel.php';
+        $response = PostsModel\updateOneById($connexion, $id, $data);
+        header('Location: ' . BASE_PUBLIC_URL . 'posts.html');
+}
+
+function editFormAction(PDO $connexion, int $id)
+{
+    include_once '../app/models/postsModel.php';
+    $post = \App\Models\PostsModel\findOneById($connexion, $id);
+    global $content, $title;
+    $title = "Alex Parker - " . $post['title'] . ROUTE_POST_EDIT_FORM;
+    ob_start();
+    include '../app/views/posts/editForm.php';
+    $content = ob_get_clean();
+}
+
+function deleteAction(PDO $connexion, int $id) {
+    include_once "../app/models/postsModel.php";
+    $response = PostsModel\deleteOneById($connexion, $id);
+    header('Location: ' . BASE_PUBLIC_URL . 'posts.html');
+    exit();
+}
